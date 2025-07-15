f79a89c4a064efd722523d2586c8b0eb2bdd9636
2320612 <prabhat3055@gmail.com>
2025-07-15 14:57:58 +0530

    app() was in main.py , which is a module objec api/aap.py thus not calleable, fixed with app.run()

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def main():
 	elif cmd=="review":
 		stats_viewer.show_review()
 	elif cmd=="webapp":
-		app()			# Run the flask localhost app
+		app.run()			# Run the flask localhost app
 	else:
 		print_help()
 if __name__=="__main__":
